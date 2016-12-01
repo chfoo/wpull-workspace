149d85bc660b96242fe049069636f191546336b7
Christopher Foo <chris.foo@gmail.com>
2016-12-01 23:11:18 +0800

    cert: Fix path

--- wbull/cert/__init__.py
+++ wbull/cert/__init__.py
@@ -6,7 +6,7 @@ import wbull.util
 
 def load_self_signed_cert(ssl_context: ssl.SSLContext):
     cert_dir = os.path.dirname(__file__)
-    cert_path = wbull.util.get_package_data_path('data/client.crt', cert_dir)
-    key_path = wbull.util.get_package_data_path('data/client.key', cert_dir)
+    cert_path = wbull.util.get_package_data_path('data/mitm_client.crt', cert_dir)
+    key_path = wbull.util.get_package_data_path('data/mitm_client.key', cert_dir)
 
     ssl_context.load_cert_chain(cert_path, key_path)
